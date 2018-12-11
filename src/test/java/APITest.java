e51d26c5b5af69b263511392f31bd5f58d270f71
Emmanuel Pius-Ogiji <Emmanuel.Ogiji@ecs-digital.co.uk>
2018-12-11 16:50:50 +0000

    m

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,54 +12,69 @@ import java.util.Properties;
 import static io.restassured.RestAssured.given;
 
 public class APITest {
-
-
-    Properties prop = new Properties();
-
-    @BeforeTest
-    public void getEnvironment(){
-        try{
-            FileInputStream fis = new FileInputStream("src/Files/env.properties");
-            prop.load(fis);
-        }
-        catch(FileNotFoundException e){
-            System.out.println("File not found");
-        }
-        catch(IOException e) {
-            System.out.println("IO Exception");
-        }
-
-    }
-
     @Test
-    public void getServer(){
+    public static void Test(){
 
-        RestAssured.baseURI = prop.getProperty("HOST");
+        RestAssured.baseURI = "https://food2fork.com";
 
-        Response res = given().auth().basic(prop.getProperty("Username"),prop.getProperty("Password")).
+        given().
+                param("key","e69f7e431a76291c119787efbe106e6d").
+                param("q","pasta").
+                header("Content-type","application/json").
+                header("Accept","application/json").
                 when().
-                get("/app/rest/server").
+                get("/api/search").
                 then().
-                assertThat().statusCode(200).extract().response();
-
-        String output = res.asString();
-        System.out.println(output);
+                assertThat().statusCode(200).contentType(ContentType.HTML);
 
     }
 
-    @Test
-    public void getProjects(){
-
-        RestAssured.baseURI = prop.getProperty("HOST");
-
-        Response res = given().auth().basic(prop.getProperty("Username"),prop.getProperty("Password")).
-                when().
-                get("/app/rest/projects").
-                then().
-                assertThat().statusCode(200).extract().response();
-
-        String output = res.asString();
-        System.out.println(output);
-
-    }
+//    Properties prop = new Properties();
+//
+//    @BeforeTest
+//    public void getEnvironment(){
+//        try{
+//            FileInputStream fis = new FileInputStream("src/Files/env.properties");
+//            prop.load(fis);
+//        }
+//        catch(FileNotFoundException e){
+//            System.out.println("File not found");
+//        }
+//        catch(IOException e) {
+//            System.out.println("IO Exception");
+//        }
+//
+//    }
+//
+//    @Test
+//    public void getServer(){
+//
+//        RestAssured.baseURI = prop.getProperty("HOST");
+//
+//        Response res = given().auth().basic(prop.getProperty("Username"),prop.getProperty("Password")).
+//                when().
+//                get("/app/rest/server").
+//                then().
+//                assertThat().statusCode(200).extract().response();
+//
+//        String output = res.asString();
+//        System.out.println(output);
+//
+//    }
+//
+//    @Test
+//    public void getProjects(){
+//
+//        RestAssured.baseURI = prop.getProperty("HOST");
+//
+//        Response res = given().auth().basic(prop.getProperty("Username"),prop.getProperty("Password")).
+//                when().
+//                get("/app/rest/projects").
+//                then().
+//                assertThat().statusCode(200).extract().response();
+//
+//        String output = res.asString();
+//        System.out.println(output);
+//
+//    }
 }
